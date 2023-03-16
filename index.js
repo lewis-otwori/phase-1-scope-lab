13ff6c27477233972cdc42d1416e323e1b9f7e8d
lewis-otwori <lewisotwori@gmail.com>
2023-03-16 07:12:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+// Declare a variable in global scope called customerName using the var keyword.
+var customerName = "bob";
+
+// Write a function that accesses that global customerName variable, and uppercases it.
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Write a function that when called, declares a variable called bestCustomer in global scope and assigns it to be 'not bob'.
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Karen";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Bob"; 
+}
